cb668ec6725219498b153c08b7a5365612cfff73
Dishav2 <105136321+Dishav2@users.noreply.github.com>
2022-08-18 10:57:10 -0500

    commit

--- pages/pages/forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm
-from .models import *
-
-class Metratr116Form(ModelForm):
-	class Meta:
-		model = Metratr116
-		fields = ['tr_id'] ##using all the fields of model "trains_display"
-		
