03550a0d3550a1cd3a77350be9e324c6832702cd
Dishav2 <105136321+Dishav2@users.noreply.github.com>
2022-08-22 10:09:58 -0500

    commit

--- config_main/pages/urls.py
+++ config_main/pages/urls.py
@@ -1,7 +1,9 @@
 from django.urls import include, path
+from django.conf.urls import url
 from . import views
 from .views import *
 from django.conf import settings
+from django.views.static import serve
 from django.conf.urls.static import static
 urlpatterns = [
     path('', LoginPageView.as_view(), name='login'),
@@ -12,7 +14,9 @@ urlpatterns = [
     path('trainspec/', views.TrainSpecView, name = 'trainspec'),
     path('trainspec/<str:train_pk>/', views.TrainSpecFilterView, name = 'trainspec'),
     path('mainscreen/', views.MainScreenView, name = 'mainscreen'),
-    
+ 
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':  settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
     ]
 
 
