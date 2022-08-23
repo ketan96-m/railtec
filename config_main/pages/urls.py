976983604bda6259df31add5afa519f4449002bc
Dishav2 <105136321+Dishav2@users.noreply.github.com>
2022-08-23 14:28:13 -0500

    commit

--- config_main/pages/urls.py
+++ config_main/pages/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 from .views import *
 from django.conf import settings
-
+from django.views.static import serve
 from django.conf.urls.static import static
 urlpatterns = [
     path('', LoginPageView.as_view(), name='login'),
@@ -16,8 +16,7 @@ urlpatterns = [
     path('mainscreen/', views.MainScreenView, name = 'mainscreen'),
  
     #url(r'^media/(?P<path>.*)$', serve,{'document_root':settings.MEDIA_ROOT}), 
-    #url(r'^static/(?P<path>.*)$', serve,{'document_root':settings.STATIC_ROOT}),
-    url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root':settings.STATIC_ROOT}),
     
     ]
 
