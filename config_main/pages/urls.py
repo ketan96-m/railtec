1819afc646c64b1b486ba7e0e1c9fc7b6dad1e7e
Dishav2 <105136321+Dishav2@users.noreply.github.com>
2022-08-23 14:12:07 -0500

    commit

--- config_main/pages/urls.py
+++ config_main/pages/urls.py
@@ -16,7 +16,7 @@ urlpatterns = [
     path('mainscreen/', views.MainScreenView, name = 'mainscreen'),
  
     #url(r'^media/(?P<path>.*)$', serve,{'document_root':settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root':settings.STATIC_ROOT}),
+    url('^static/(?P<path>.*)$', serve,{'document_root':settings.STATIC_ROOT}),
     ]
 
 
