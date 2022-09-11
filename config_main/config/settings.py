258fc7e5fc52fadc3b52155cacf9c3e60a679901
Dishav2 <105136321+Dishav2@users.noreply.github.com>
2022-09-11 10:10:50 -0500

    Update settings.py

--- config_main/config/settings.py
+++ config_main/config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-v^wpfjv_apl9)_zi8q@ku&@0p3f%g#!t^sz_75ba4^!#g2ef+!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['54.196.198.18', 'FTA.railtec.illinois.edu', 'ftarailtec.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['54.196.198.18', 'fta.railtec.illinois.edu', 'ftarailtec.herokuapp.com','127.0.0.1']
 
 # Application definition
 
